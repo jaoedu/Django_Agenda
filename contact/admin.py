d5a5d36ceff71f488666527773a07f8a71423659
João Mota <ejoao282@gmail.com>
2023-11-08 14:58:29 -0300

    models created and start config admin page

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from contact  import models
 
-# Register your models here.
+# Register your models here.]
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id' , 'first_name', 'last_name', 'phone')
+    ordering = ('-id' ,)
+    search_fields = ('id', 'first_name' , 'last_name',)
+    list_per_page = 1
+    list_editable = ('first_name' , 'last_name')
+    
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length= 40)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=255)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank= True)
+    
+    def __str__(self):
+        return f'{self.first_name}  {self.last_name}'
+    
